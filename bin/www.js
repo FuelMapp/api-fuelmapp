51e1a7f8a27ca500fa9cb091c3630c1700bbf83e
TL-Garcia <tomas.bl.garcia@gmail.com>
2021-02-24 15:42:28 +0100

    chore: Clean boilerplate files

---
+++ bin/www.js
@@ -0,0 +1,9 @@
+const app = require('../app');
+const http = require('http');
+const {normalizePort} = require('../utils')
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
